d56149a1621ae6db1d9b1175d810e3617b1f021d
nicola01zullo@gmail.com <Temperamatite99>
2023-01-25 19:45:10 +0100

    Modifica logica di accesso e registrazione

---
+++ src/main/java/controller/AccessoUtente.java
@@ -0,0 +1,69 @@
+package controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ConPool;
+import model.UtenteBean;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@WebServlet(urlPatterns = "/accesso-utente-servlet")
+public class AccessoUtente extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        //verifica se l'utente è registrato oppure no
+        try (Connection con = ConPool.getConnection()) {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user WHERE Username = '"+username+"' AND Password = '"+password + "'");
+            if(rs.next()){
+                if (rs.getBoolean(7)==true){
+                    /*Utente Admin*/
+                        UtenteBean utenteLoggato = new UtenteBean();
+                        utenteLoggato.setId(rs.getInt(1));
+                        utenteLoggato.setUsername(rs.getString(2));
+                        utenteLoggato.setName(rs.getString(3));
+                        utenteLoggato.setSurname(rs.getString(4));
+                        utenteLoggato.setEmail(rs.getString(5));
+                        utenteLoggato.setPassword(rs.getString(6));
+                        utenteLoggato.setRuolo(true);
+                        request.getSession().setAttribute("utenteLoggato", utenteLoggato);
+                        RequestDispatcher dispatcher = request.getRequestDispatcher("areaAdmin.jsp");
+                        dispatcher.forward(request, response);
+                    /*Utente Admin*/
+                }else{
+                    UtenteBean utenteLoggato = new UtenteBean();
+                    utenteLoggato.setId(rs.getInt(1));
+                    utenteLoggato.setName(rs.getString(2));
+                    utenteLoggato.setSurname(rs.getString(3));
+                    utenteLoggato.setEmail(rs.getString(4));
+                    utenteLoggato.setPassword(rs.getString(5));
+                    request.getSession().setAttribute("utenteLoggato", utenteLoggato);
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+                    dispatcher.forward(request, response);
+                }
+            }else {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+                dispatcher.forward(request, response);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ target/Tommit-1.0-SNAPSHOT/login.jsp
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'><link rel="stylesheet" href="../css/navbar.css">
+
+  <link rel="stylesheet" href="static/css/login.css">
+  <link rel="stylesheet" href="static/css/background.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+<div class="bg-img">
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Appunti</a></li>
+        <li><a href="#">Gruppi Studio</a></li>
+        <li><a href="#">Timer</a></li>
+        <li><a href="#">icona</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="content">
+
+  <div class="form-wrapper">
+
+    <div class="linker">
+
+      <span class="ring"></span>
+      <span class="ring"></span>
+      <span class="ring"></span>
+      <span class="ring"></span>
+      <span class="ring"></span>
+    </div>
+
+    <form class="login-form" action="accesso-utente-servlet" method="post">
+      <input type="text" name="username" placeholder="username" />
+      <input type="password" name="password" placeholder="password" />
+      <button type="submit">Log in</button>
+      <a href="#">Registrati!</a>
+
+    </form>
+  </div>
+</div>
+</div>
+</body>
+</html>
