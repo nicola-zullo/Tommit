e98cb00437df26dba18989f86f26f62ca808dff0
Marco Guerrera <m.guerrera3@studenti.unisa.it>
2023-01-30 11:47:41 +0100

    Update TimerDAO con metodi showAllTimer e deleteTimer

--- src/main/java/model/TimerDAO.java
+++ src/main/java/model/TimerDAO.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class TimerDAO {
 
@@ -26,4 +27,38 @@ public class TimerDAO {
         }
     }
 
+
+    public void deleteTimer(TimerBean timer){
+        Statement st;
+        try (Connection con = ConPool.getConnection()) {
+            st= con.createStatement();
+            st.executeUpdate("delete from timer where nome like '%"+timer.getName()+"%' and userId like '%"+timer.getUtenteId()+"%' and session like '%"+timer.getSessionTime()+"%' and break like '%"+timer.getBreakTime()+"%'");
+            return;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<TimerBean>showAllTimer(String userID)
+    {
+        ArrayList<TimerBean> list = new ArrayList<>();
+        try (Connection con = ConPool.getConnection())
+        {
+            PreparedStatement ps = con.prepareStatement("select * from timer where `userId` like '%"+userID+"%'");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next())
+            {
+                TimerBean timer = new TimerBean();
+                timer.setName(rs.getString("nome"));
+                timer.setBreakTime((rs.getInt("break")));
+                timer.setSessionTime(rs.getInt("session"));
+                timer.setUtenteId(rs.getInt("uerId"));
+                list.add(timer);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
