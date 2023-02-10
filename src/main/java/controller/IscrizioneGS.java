0e78ff4720008a8b00ade55d1c70604e09da9b83
nicola01zullo@gmail.com <Temperamatite99>
2023-02-10 12:17:25 +0100

    Inserita logica iscrizione gs

---
+++ src/main/java/controller/IscrizioneGS.java
@@ -0,0 +1,29 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.UtenteBean;
+import model.UtentiGSDAO;
+
+import java.io.IOException;
+
+@WebServlet(name="iscrizione-gs", value ="/iscrizione-gs")
+public class IscrizioneGS extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            UtenteBean u = (UtenteBean) request.getSession().getAttribute("utenteLoggato");
+            int id_utente = u.getId();
+            String nome_gs = request.getParameter("nome");//prendere da un hidden param nome grupposStudio
+
+        UtentiGSDAO utentiGSDAO = new UtentiGSDAO();
+
+        utentiGSDAO.doSave(id_utente,nome_gs);
+
+    }
+
+}
